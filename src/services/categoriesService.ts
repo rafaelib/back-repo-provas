765de6d73194ab3c511a7a468ed0d9babc4c8f48
thiagoribb <thiribeiro142@gmail.com>
2021-08-09 16:46:57 -0300

    Preparando revisão de código

--- src/services/categoriesService.ts
+++
@@ -1,7 +0,0 @@
-import { getRepository } from "typeorm";
-import Category from "../entities/Category";
-
-export async function getCategories(): Promise<Category[]> {
-  const categories = await getRepository(Category).find();
-  return categories;
-}
